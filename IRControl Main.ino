acafbf5434a609fc094360979c6fe97cfc9093c9
MichaelaLuna <116026170+MichaelaLuna@users.noreply.github.com>
2023-05-20 15:51:14 -0400

    Update IRControl Main.ino

--- IRControl Main.ino
+++ IRControl Main.ino
@@ -3,6 +3,13 @@
 #include "IrControl.h"  // Include the "IrControl.h" header file
 #include <Arduino.h>    // Include the "Arduino.h" header file
 
+#define motorA 7          // Define motorA to this pin#
+#define motorB 8          // Define motorB to this pin#
+#define motorSpeedA 5     // Define motorSpeedA to this pin#
+#define motorSpeedB 6     // Define motorSpeedB to this pin#
+#define enablePin 3       // Define enablePin to this pin#
+#define receiverPin 9     // Define receiverPins to this pin#
+
 IrControl Ir (receiverPin, motorA, motorB, motorSpeedA, motorSpeedB, enablePin);  // Initialize the object with the corresponding pin numbers
 
 void setup() {          // Run the code once
