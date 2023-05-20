ba417f6040099eec05a744521fa9002a4570132d
MichaelaLuna <116026170+MichaelaLuna@users.noreply.github.com>
2023-05-20 15:46:24 -0400

    Update IRControl Main.ino

--- IRControl Main.ino
+++ IRControl Main.ino
@@ -3,7 +3,7 @@
 #include "IrControl.h"  // Include the "IrControl.h" header file
 #include <Arduino.h>    // Include the "Arduino.h" header file
 
-IrControl Ir(9, 7, 8, 5, 6, 3);  // Initialize the object with the corresponding pin numbers
+IrControl Ir (receiverPin, motorA, motorB, motorSpeedA, motorSpeedB, enablePin);  // Initialize the object with the corresponding pin numbers
 
 void setup() {          // Run the code once
   Ir.init();            // Initialization Method
